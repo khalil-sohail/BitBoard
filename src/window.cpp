456fdac1447e4773582f02f5d2f60ebe28d49dc8
ERROR244 <khalilsohail24@gmail.com>
2024-12-12 18:34:07 +0100

    change mlx library

--- src/window.cpp
+++ src/window.cpp
@@ -5,17 +5,3 @@ int	closeWindow()
     exit(EXIT_SUCCESS);
     return (0);
 }
-
-// int handle_esc_key(int keycode) {
-//     // (void)param;
-//     if (keycode == 65307) {
-//         std::cout << "Escape key pressed. Exiting program.\n";
-//         exit(0);
-//     }
-//     return 0;
-// }
-
-// int handle_esc_key() {
-//     std::cout << "Escape key pressed. Exiting program.\n";
-//     return 0;
-// }
